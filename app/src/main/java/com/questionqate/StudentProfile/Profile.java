b7d6b8a3d39221d121bb00172f0111a1ebb5c2a2
wardah9 <wardah9658@gmail.com>
2017-11-23 16:55:15 +0400

    working with profile thing again ..

--- app/src/main/java/com/questionqate/StudentProfile/Profile.java
+++ app/src/main/java/com/questionqate/StudentProfile/Profile.java
@@ -1,22 +1,37 @@
 package com.questionqate.StudentProfile;
 
+import android.Manifest;
 import android.app.ActionBar;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.questionqate.R;
 import com.squareup.picasso.Picasso;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Profile extends AppCompatActivity {
 
-    String url_img = "https://firebasestorage.googleapis.com/v0/b/questionsqate-9a3d7.appspot.com/o/girl.png?alt=media&token=f9831356-447e-4de9-b5cf-7d45b169c056";
     ImageView profile_image;
     TextView profile_name;
     TextView profile_mail;
@@ -25,22 +40,16 @@ public class Profile extends AppCompatActivity {
     EditText profile_mail_edt;
     EditText profile_phone_edt;
 
+
+    private int SELECT_FROM_GALLARY = 2;
+    private int SELECT_FROM_CAMERA = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayShowCustomEnabled(true);
-            LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            if (inflator != null) {
-                View v = inflator.inflate(R.layout.card_view_questin, null);
-                actionBar.setCustomView(v);
-            }
-        }
         profile_image = findViewById(R.id.profile_image);
-        Picasso.with(this).load(url_img).into(profile_image);
 
         profile_phone = findViewById(R.id.profile_phone);
         profile_mail = findViewById(R.id.profile_mail);
@@ -50,6 +59,97 @@ public class Profile extends AppCompatActivity {
         profile_mail_edt = findViewById(R.id.profile_mail_edt);
         profile_phone_edt = findViewById(R.id.profile_phone_edt);
 
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowCustomEnabled(true);
+            LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (inflator != null) {
+                View v = inflator.inflate(R.layout.ic_edit, null);
+
+                ImageView edit_pro = v.findViewById(R.id.edit_pro);
+                edit_pro.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+//                        Toast.makeText(Profile.this, "edit icon", Toast.LENGTH_SHORT).show();
+                        profile_phone.setVisibility(View.GONE);
+                        profile_mail.setVisibility(View.GONE);
+                        profile_name.setVisibility(View.GONE);
+
+                        profile_name_edt.setVisibility(View.VISIBLE);
+                        profile_mail_edt.setVisibility(View.VISIBLE);
+                        profile_phone_edt.setVisibility(View.VISIBLE);
+
+                    }
+                });
+                actionBar.setCustomView(v);
+            }
+        }
+
+
+        profile_image.setOnClickListener(view ->
+        {
+
+            final Dialog dialog = new Dialog(Profile.this);
+            dialog.setContentView(R.layout.custom_dialog_ex2);
+            Button camera_button = (Button) dialog.findViewById(R.id.cc);
+            Button gallery_button = (Button) dialog.findViewById(R.id.gg);
+
+            camera_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if (ContextCompat.checkSelfPermission(Profile.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(Profile.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+                    } else {
+                        Intent cameraIntent = new Intent();
+                        cameraIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, SELECT_FROM_CAMERA);
+                    }
+
+                    dialog.dismiss();
+                }
+            });
+            gallery_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, SELECT_FROM_GALLARY);
+
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
+
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == SELECT_FROM_CAMERA && resultCode == RESULT_OK) {
+
+            Bitmap cameraBitmap = (Bitmap) data.getExtras().get("data");
+            profile_image.setImageBitmap(cameraBitmap);
+        } else if (requestCode == SELECT_FROM_GALLARY && resultCode == RESULT_OK) {
+
+            try {
+                InputStream input = getContentResolver().openInputStream(data == null ? null : data.getData());
+                final Bitmap bitmap = BitmapFactory.decodeStream(input, null, new BitmapFactory.Options());
+
+                Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                profile_image.setImageDrawable(drawable);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(this, "Error occurred ! ,  " + resultCode, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }

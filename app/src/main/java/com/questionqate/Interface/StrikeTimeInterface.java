50e80198b9d121daf52dfc1c3cfae431c9b9b8c5
anwar salim <anwar@css.edu.om>
2017-11-25 13:25:00 +0400

    latest development

---
+++ app/src/main/java/com/questionqate/Interface/StrikeTimeInterface.java
@@ -0,0 +1,10 @@
+package com.questionqate.Interface;
+
+/**
+ * Created by CodeCrazy on 11/24/17.
+ */
+
+public interface StrikeTimeInterface {
+    void onStrike(int time);
+    void RemoveStrike();
+}
